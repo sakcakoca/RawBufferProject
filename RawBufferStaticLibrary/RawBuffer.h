27a0d33345ea14f5d5912cac22055d4002f480e6
Salim AKCAKOCA <salim.akcakoca@msn.com>
2021-04-25 21:57:37 +0300

    osx fix

--- RawBufferStaticLibrary/RawBuffer.h
+++ RawBufferStaticLibrary/RawBuffer.h
@@ -15,14 +15,14 @@ concept OneByteData = sizeof(T) == 1;
 class RawBuffer
 {
 public:
-	inline RawBuffer();
+	RawBuffer();
 	
-	inline void operator+=(OneByteData auto data);
+	void operator+=(OneByteData auto data);
 	
-	inline void operator+=(const RawBuffer& otherBuffer);
+	void operator+=(const RawBuffer& otherBuffer);
 	
 	template<typename T> requires OneByteData<T>
-	inline void operator+=(const std::vector<T>& vector);
+	void operator+=(const std::vector<T>& vector);
 	
 	void add(OneByteData auto data);
 	
@@ -37,46 +37,3 @@ private:
 	
 	std::vector<std::byte> buffer;
 };
-
-inline RawBuffer::RawBuffer()
-{
-	buffer.reserve(INITIAL_SIZE_OF_BUFFER);
-}
-
-
-const auto& RawBuffer::getInternalBuffer() const
-{
-	return buffer;
-}
-
-size_t RawBuffer::getBufferCapacity() const
-{
-	return buffer.capacity();
-}
-
-size_t RawBuffer::getSize() const
-{
-	return buffer.size();
-}
-
-void RawBuffer::operator+=(const RawBuffer& otherBuffer)
-{
-	buffer.insert(buffer.end(), otherBuffer.getInternalBuffer().begin(), otherBuffer.getInternalBuffer().end());
-}
-
-template <typename T> requires OneByteData<T>
-void RawBuffer::operator+=(const std::vector<T>& vector)
-{
-	buffer.insert(buffer.end(), vector.begin(), vector.end());
-}
-
-void RawBuffer::operator+=(OneByteData auto data)
-{
-	add(data);
-}
-
-void RawBuffer::add(OneByteData auto data)
-{
-	std::cout << "Size: " << sizeof(data) << std::endl;
-	buffer.push_back(static_cast<std::byte>(data));
-}
