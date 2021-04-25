9bacdaea514a4755078b178e7f6afe05329d9f5b
Salim AKCAKOCA <salim.akcakoca@msn.com>
2021-04-25 15:45:58 +0300

    Removed commented code

--- RawBufferUser/RawBufferUser.cpp
+++ RawBufferUser/RawBufferUser.cpp
@@ -24,14 +24,6 @@ void printCompilerInfo()
 int main()
 {
 	printCompilerInfo();
-	/**
-	std::vector<int> v{ 1, 2, 3};
-	auto two = std::find(v.begin(), v.end(), 2);
-	for(int i = 0; i < 4096; i++)
-	{
-		v.push_back(i);
-	}
-	std::cout << *two << std::endl;
-	*/
+	
 	return 0;
 }
